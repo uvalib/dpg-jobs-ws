907b41cc4184cbb1458e04a7f0558b47f251da9e
Dave Goldstein <dpg3k@virginia.edu>
2022-04-07 07:25:59 -0400

    Correct binary name

--- package/scripts/entry.sh
+++ package/scripts/entry.sh
@@ -5,7 +5,8 @@
 
 # run the server
 umask 0002
-cd bin; ./imagingsvc -url $DPG_SERVICE_URL -images $DPG_IMAGE_PATH -scan $DPG_SCAN_PATH -finalize $DPG_FINALIZE_PATH -iiif $IIIF_SERVICE_URL -jwtkey $DPG_JWT_KEY -dbhost $DBHOST -dbport $DBPORT -dbname $DBNAME -dbuser $DBUSER -dbpass $DBPASS
+cd bin; ./dpg-jobs-ws
+# -url $DPG_SERVICE_URL -images $DPG_IMAGE_PATH -scan $DPG_SCAN_PATH -finalize $DPG_FINALIZE_PATH -iiif $IIIF_SERVICE_URL -jwtkey $DPG_JWT_KEY -dbhost $DBHOST -dbport $DBPORT -dbname $DBNAME -dbuser $DBUSER -dbpass $DBPASS
 
 # return the status
 exit $?
