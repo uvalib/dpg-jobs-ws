727b5cf0ba375e3f890b11cbe6a65d131c6623ab
Lou Foster <louffoster@gmail.com>
2022-04-28 16:05:03 -0400

    tweak ocr callback resp struct to (hopefully) match what the service is returning

--- cmd/ocr.go
+++ cmd/ocr.go
@@ -74,11 +74,13 @@ func (svc *ServiceContext) ocrDoneCallback(c *gin.Context) {
 	}
 
 	svc.logInfo(&pendingJob, "Received OCR callback")
-	cbResp := struct {
-		Status  string `json:"status"`
-		Message string `json:"message"`
-	}{}
-
+	type ocrRespData struct {
+		JSON struct {
+			Status  string `json:"status"`
+			Message string `json:"message"`
+		} `json:"json"`
+	}
+	var cbResp ocrRespData
 	qpErr := c.ShouldBindJSON(&cbResp)
 	if qpErr != nil {
 		log.Printf("ERROR: invalid OCR callback payload: %s", qpErr.Error())
@@ -86,10 +88,10 @@ func (svc *ServiceContext) ocrDoneCallback(c *gin.Context) {
 		return
 	}
 
-	if cbResp.Status == "success" {
+	if cbResp.JSON.Status == "success" {
 		svc.logInfo(&pendingJob, "OCR request completed successfully")
 	} else {
-		svc.logInfo(&pendingJob, fmt.Sprintf("OCR request failed: %s", cbResp.Message))
+		svc.logInfo(&pendingJob, fmt.Sprintf("OCR request failed: %s", cbResp.JSON.Message))
 	}
 
 	c.String(http.StatusOK, "ok")
