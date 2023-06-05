a6f1dd03a8262a338cff6f36b3ad24680a9e14c8
Lou Foster <louffoster@gmail.com>
2023-06-05 15:56:05 -0400

    switch hathi package code to work with the new api layout (POST with json payload that can accept multiple items to process)

--- cmd/hathitrust.go
+++ cmd/hathitrust.go
@@ -11,7 +11,6 @@ import (
 	"os"
 	"path"
 	"runtime/debug"
-	"strconv"
 	"strings"
 	"time"
 
@@ -69,7 +68,7 @@ func (svc *ServiceContext) submitHathiTrustMetadata(c *gin.Context) {
 		return
 	}
 
-	js, err := svc.createJobStatus("HathiTrust", "StaffMember", submitUser.ID)
+	js, err := svc.createJobStatus("HathiTrustMetadata", "StaffMember", submitUser.ID)
 	if err != nil {
 		log.Printf("ERROR: unable to create HathiTrush metadata job status: %s", err.Error())
 		c.String(http.StatusInternalServerError, err.Error())
@@ -192,58 +191,44 @@ func (svc *ServiceContext) submitHathiTrustMetadata(c *gin.Context) {
 }
 
 func (svc *ServiceContext) createHathiTrustPackage(c *gin.Context) {
-	mdID, _ := strconv.ParseInt(c.Param("id"), 10, 64)
-	js, err := svc.createJobStatus("HathiTrust", "Metadata", mdID)
+	log.Printf("INFO: received hathitrust package request")
+	var req hathiTrustRequest
+	err := c.ShouldBindJSON(&req)
+	if err != nil {
+		log.Printf("ERROR: unable to parse hathitrust package request: %s", err.Error())
+		c.String(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	if req.ComputeID == "" {
+		log.Printf("ERROR: hathitrust package request requires compute id")
+		c.String(http.StatusBadRequest, "compute id is required")
+		return
+	}
+	if len(req.MetadataIDs) == 0 {
+		log.Printf("INFO: hathitrust package request requires metadata ids")
+		c.String(http.StatusBadRequest, "metadata id list is required")
+		return
+	}
+
+	var submitUser staffMember
+	err = svc.GDB.Where("computing_id=?", req.ComputeID).First(&submitUser).Error
+	if err != nil {
+		log.Printf("ERROR: user %s not found: %s", req.ComputeID, err.Error())
+		c.String(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	if submitUser.Role != 0 {
+		log.Printf("ERROR: hathitrust package requests can only be submitted by admin users")
+		c.String(http.StatusBadRequest, "you do not have permission to make this request")
+		return
+	}
+
+	js, err := svc.createJobStatus("HathiTrustPackage", "StaffMember", submitUser.ID)
 	if err != nil {
 		log.Printf("ERROR: unable to create HathiTrush package job status: %s", err.Error())
 		c.String(http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	svc.logInfo(js, fmt.Sprintf("Validate metadata record %d", mdID))
-	var md metadata
-	err = svc.GDB.Preload("OcrHint").Find(&md, mdID).Error
-	if err != nil {
-		svc.logFatal(js, fmt.Sprintf("Unable to get metadata %d: %s", mdID, err.Error()))
-		return
-	}
-
-	if md.OcrHint == nil {
-		svc.logFatal(js, "Metadata is missing the required OCRHint setting")
-		return
-	}
-
-	svc.logInfo(js, "Find target unit")
-	var units []unit
-	err = svc.GDB.Where("metadata_id=? and (intended_use_id=? or intended_use_id=?) and date_dl_deliverables_ready is not null", mdID, 110, 101).Find(&units).Error
-	if err != nil {
-		svc.logFatal(js, fmt.Sprintf("Unable to get a digitial collection building unit for metadata %d: %s", mdID, err.Error()))
-		return
-	}
-
-	if len(units) > 1 {
-		svc.logFatal(js, fmt.Sprintf("Too many units found (%d) for metadata %d", len(units), mdID))
-		return
-	}
-
-	if len(units) == 0 {
-		svc.logFatal(js, fmt.Sprintf("No units found for metadata %d", mdID))
-		return
-	}
-
-	tgtUnit := units[0]
-	tgtUnit.Metadata = &md
-	svc.logInfo(js, fmt.Sprintf("Create HathiTrust submission package for metadata %s unit %d", md.PID, tgtUnit.ID))
-
-	svc.logInfo(js, "Load master files for unit")
-	var masterFiles []masterFile
-	err = svc.GDB.Where("unit_id=?", tgtUnit.ID).Find(&masterFiles).Error
-	if err != nil {
-		svc.logFatal(js, fmt.Sprintf("Unabe to load master files for unit %d: %s", tgtUnit.ID, err.Error()))
-		return
-	}
-	if len(masterFiles) == 0 {
-		svc.logFatal(js, fmt.Sprintf("No master files found for unit %d", tgtUnit.ID))
 		return
 	}
 
@@ -256,120 +241,179 @@ func (svc *ServiceContext) createHathiTrustPackage(c *gin.Context) {
 			}
 		}()
 
-		// Setup all working directories; /digiserv-production/hathitrust/[barcode]/content
-		// final package data will reside in /digiserv-production/hathitrust/[barcode]
-		packageDir := path.Join(svc.ProcessingDir, "hathitrust", md.Barcode)
-		assembleDir := path.Join(packageDir, "content")
-		packageName := fmt.Sprintf("%s.zip", md.Barcode)
-		packageFilename := path.Join(packageDir, packageName)
-		if pathExists(packageDir) {
-			svc.logInfo(js, fmt.Sprintf("Clean up pre-existing package assembly directory %s", packageDir))
-			err := os.RemoveAll(packageDir)
+		for _, mdID := range req.MetadataIDs {
+			svc.logInfo(js, fmt.Sprintf("Validate metadata record %d", mdID))
+			var md metadata
+			err = svc.GDB.Preload("OcrHint").Find(&md, mdID).Error
 			if err != nil {
-				svc.logFatal(js, fmt.Sprintf("Unable to cleanup prior package assembly directory %s: %s", packageDir, err.Error()))
-				return
+				svc.logError(js, fmt.Sprintf("Unable to get metadata %d: %s", mdID, err.Error()))
+				continue
 			}
-		}
-		err = ensureDirExists(assembleDir, 0777)
-		if err != nil {
-			svc.logFatal(js, fmt.Sprintf("Unable to create package / assembly directories %s: %s", assembleDir, err.Error()))
-			return
-		}
 
-		// If OCR is applicable, perform it first
-		if md.OcrHint.OcrCandidate {
-			svc.logInfo(js, "This unit is an OCR candidate; check master files to see if OCR needs to be done")
-			doOCR := true
-			for _, mf := range masterFiles {
-				if mf.TranscriptionText != "" {
-					svc.logInfo(js, fmt.Sprintf("Masterfile %s:%s has OCR text. Assume the whole unit has been OCR'd already", mf.PID, mf.Filename))
-					doOCR = false
+			if md.OcrHint == nil {
+				svc.logError(js, "Metadata is missing the required OCRHint setting")
+				continue
+			}
+
+			svc.logInfo(js, "Find target unit")
+			var units []unit
+			err = svc.GDB.Where("metadata_id=? and (intended_use_id=? or intended_use_id=?) and date_dl_deliverables_ready is not null", mdID, 110, 101).Find(&units).Error
+			if err != nil {
+				svc.logError(js, fmt.Sprintf("Unable to get a digitial collection building unit for metadata %d: %s", mdID, err.Error()))
+				continue
+			}
+
+			if len(units) > 1 {
+				svc.logError(js, fmt.Sprintf("Too many units found (%d) for metadata %d", len(units), mdID))
+				continue
+			}
+
+			if len(units) == 0 {
+				svc.logError(js, fmt.Sprintf("No units found for metadata %d", mdID))
+				continue
+			}
+
+			tgtUnit := units[0]
+			tgtUnit.Metadata = &md
+			svc.logInfo(js, fmt.Sprintf("Create HathiTrust submission package for metadata %s unit %d", md.PID, tgtUnit.ID))
+
+			svc.logInfo(js, "Load master files for unit")
+			var masterFiles []masterFile
+			err = svc.GDB.Where("unit_id=?", tgtUnit.ID).Find(&masterFiles).Error
+			if err != nil {
+				svc.logError(js, fmt.Sprintf("Unabe to load master files for unit %d: %s", tgtUnit.ID, err.Error()))
+				continue
+			}
+			if len(masterFiles) == 0 {
+				svc.logError(js, fmt.Sprintf("No master files found for unit %d", tgtUnit.ID))
+				continue
+			}
+
+			// Setup all working directories; /digiserv-production/hathitrust/[barcode]/content
+			// final package data will reside in /digiserv-production/hathitrust/[barcode]
+			packageDir := path.Join(svc.ProcessingDir, "hathitrust", md.Barcode)
+			assembleDir := path.Join(packageDir, "content")
+			packageName := fmt.Sprintf("%s.zip", md.Barcode)
+			packageFilename := path.Join(packageDir, packageName)
+			if pathExists(packageDir) {
+				svc.logInfo(js, fmt.Sprintf("Clean up pre-existing package assembly directory %s", packageDir))
+				err := os.RemoveAll(packageDir)
+				if err != nil {
+					svc.logError(js, fmt.Sprintf("Unable to cleanup prior package assembly directory %s: %s", packageDir, err.Error()))
+					continue
+				}
+			}
+			err = ensureDirExists(assembleDir, 0777)
+			if err != nil {
+				svc.logError(js, fmt.Sprintf("Unable to create package / assembly directories %s: %s", assembleDir, err.Error()))
+				continue
+			}
+
+			// If OCR is applicable, perform it first
+			if md.OcrHint.OcrCandidate {
+				svc.logInfo(js, "This unit is an OCR candidate; check master files to see if OCR needs to be done")
+				doOCR := true
+				for _, mf := range masterFiles {
+					if mf.TranscriptionText != "" {
+						svc.logInfo(js, fmt.Sprintf("Masterfile %s:%s has OCR text. Assume the whole unit has been OCR'd already", mf.PID, mf.Filename))
+						doOCR = false
+						break
+					}
+				}
+				if doOCR {
+					// note; this call will not return until all master files in the unit have OCR results
+					err = svc.requestUnitOCR(js, &tgtUnit)
+					if err != nil {
+						svc.logError(js, fmt.Sprintf("Unable to request OCR: %s", err.Error()))
+						continue
+					}
+				}
+			}
+
+			// Create the package ZIP file
+			svc.logInfo(js, fmt.Sprintf("Package will be generated here %s", packageFilename))
+			zipFile, err := os.Create(packageFilename)
+			if err != nil {
+				svc.logError(js, fmt.Sprintf("Unable to create package zip %s: %s", packageFilename, err.Error()))
+				continue
+			}
+			zipWriter := zip.NewWriter(zipFile)
+			defer zipFile.Close()
+			defer zipWriter.Close()
+
+			// Create the checksum file; it will appended as files are processed
+			checksumPath := path.Join(assembleDir, "checksum.md5")
+			checksumFile, err := os.Create(checksumPath)
+			if err != nil {
+				svc.logError(js, fmt.Sprintf("Unable to create checksum file %s: %s", checksumPath, err.Error()))
+				continue
+			}
+			checksumFile.Chmod(0666)
+			defer checksumFile.Close()
+
+			// Write the meta.yml file
+			lastCaptureDate := masterFiles[len(masterFiles)-1].CreatedAt
+			ymlMD5, err := svc.writeMetaYML(js, assembleDir, &lastCaptureDate, tgtUnit.DateDLDeliverablesReady)
+			if err != nil {
+				svc.logError(js, fmt.Sprintf("Unable to write meta.yml: %s", err.Error()))
+				continue
+			}
+			checksumFile.WriteString(fmt.Sprintf("%s  meta.yml\n", ymlMD5))
+
+			masterFileError := false
+			for idx, mf := range masterFiles {
+				// copy jp2 to assembly directory, then add it to the zip
+				destFN := fmt.Sprintf("%08d.jp2", (idx + 1))
+				jp2kInfo := svc.iiifPath(mf.PID)
+				if pathExists(jp2kInfo.absolutePath) == false {
+					svc.logFatal(js, fmt.Sprintf("MasterFile %s:%s is missing JP2 derivative %s", mf.PID, mf.Filename, jp2kInfo.absolutePath))
+					masterFileError = true
 					break
 				}
-			}
-			if doOCR {
-				// note; this call will not return until all master files in the unit have OCR results
-				err = svc.requestUnitOCR(js, &tgtUnit)
+
+				destPath := path.Join(assembleDir, destFN)
+				err = copyJP2(jp2kInfo.absolutePath, destPath)
 				if err != nil {
-					svc.logFatal(js, fmt.Sprintf("Unable to request OCR: %s", err.Error()))
-					return
+					svc.logError(js, fmt.Sprintf("Unable to copy %s to %s %s", jp2kInfo.absolutePath, destPath, err.Error()))
+					masterFileError = true
+					break
+				}
+
+				_, err := addFileToZip(packageFilename, zipWriter, assembleDir, destFN)
+				if err != nil {
+					svc.logError(js, fmt.Sprintf("Unable to add %s to zip file: %s", destPath, err.Error()))
+					masterFileError = true
+					break
+				}
+				checksumFile.WriteString(fmt.Sprintf("%s  %s\n", md5Checksum(destPath), destFN))
+
+				// if applicable, copy ocr text to the package dir. make the name match the image name
+				if md.OcrHint.OcrCandidate {
+					txtFileName := fmt.Sprintf("%08d.txt", (idx + 1))
+					destTxtPath := path.Join(assembleDir, txtFileName)
+					err = os.WriteFile(destTxtPath, []byte(mf.TranscriptionText), 0666)
+					if err != nil {
+						svc.logError(js, fmt.Sprintf("Unable to write OCR text to %s %s", destTxtPath, err.Error()))
+						masterFileError = true
+						break
+					}
+					_, err := addFileToZip(packageFilename, zipWriter, assembleDir, txtFileName)
+					if err != nil {
+						svc.logError(js, fmt.Sprintf("Unable to add %s to zip file: %s", destTxtPath, err.Error()))
+						masterFileError = true
+						break
+					}
+					checksumFile.WriteString(fmt.Sprintf("%s  %s\n", md5Checksum(destTxtPath), txtFileName))
 				}
 			}
-		}
 
-		// Create the package ZIP file
-		svc.logInfo(js, fmt.Sprintf("Package will be generated here %s", packageFilename))
-		zipFile, err := os.Create(packageFilename)
-		if err != nil {
-			svc.logFatal(js, fmt.Sprintf("Unable to create package zip %s: %s", packageFilename, err.Error()))
-			return
-		}
-		zipWriter := zip.NewWriter(zipFile)
-		defer zipFile.Close()
-		defer zipWriter.Close()
-
-		// Create the checksum file; it will appended as files are processed
-		checksumPath := path.Join(assembleDir, "checksum.md5")
-		checksumFile, err := os.Create(checksumPath)
-		if err != nil {
-			svc.logFatal(js, fmt.Sprintf("Unable to create checksum file %s: %s", checksumPath, err.Error()))
-			return
-		}
-		checksumFile.Chmod(0666)
-		defer checksumFile.Close()
-
-		// Write the meta.yml file
-		lastCaptureDate := masterFiles[len(masterFiles)-1].CreatedAt
-		ymlMD5, err := svc.writeMetaYML(js, assembleDir, &lastCaptureDate, tgtUnit.DateDLDeliverablesReady)
-		if err != nil {
-			svc.logFatal(js, fmt.Sprintf("Unable to write meta.yml: %s", err.Error()))
-			return
-		}
-		checksumFile.WriteString(fmt.Sprintf("%s  meta.yml\n", ymlMD5))
-
-		for idx, mf := range masterFiles {
-			// copy jp2 to assembly directory, then add it to the zip
-			destFN := fmt.Sprintf("%08d.jp2", (idx + 1))
-			jp2kInfo := svc.iiifPath(mf.PID)
-			if pathExists(jp2kInfo.absolutePath) == false {
-				svc.logFatal(js, fmt.Sprintf("MasterFile %s:%s is missing JP2 derivative %s", mf.PID, mf.Filename, jp2kInfo.absolutePath))
-				return
+			if masterFileError {
+				continue
 			}
 
-			destPath := path.Join(assembleDir, destFN)
-			err = copyJP2(jp2kInfo.absolutePath, destPath)
-			if err != nil {
-				svc.logFatal(js, fmt.Sprintf("Unable to copy %s to %s %s", jp2kInfo.absolutePath, destPath, err.Error()))
-				return
-			}
-
-			_, err := addFileToZip(packageFilename, zipWriter, assembleDir, destFN)
-			if err != nil {
-				svc.logFatal(js, fmt.Sprintf("Unable to add %s to zip file: %s", destPath, err.Error()))
-				return
-			}
-			checksumFile.WriteString(fmt.Sprintf("%s  %s\n", md5Checksum(destPath), destFN))
-
-			// if applicable, copy ocr text to the package dir. make the name match the image name
-			if md.OcrHint.OcrCandidate {
-				txtFileName := fmt.Sprintf("%08d.txt", (idx + 1))
-				destTxtPath := path.Join(assembleDir, txtFileName)
-				err = os.WriteFile(destTxtPath, []byte(mf.TranscriptionText), 0666)
-				if err != nil {
-					svc.logFatal(js, fmt.Sprintf("Unable to write OCR text to %s %s", destTxtPath, err.Error()))
-					return
-				}
-				_, err := addFileToZip(packageFilename, zipWriter, assembleDir, txtFileName)
-				if err != nil {
-					svc.logFatal(js, fmt.Sprintf("Unable to add %s to zip file: %s", destTxtPath, err.Error()))
-					return
-				}
-				checksumFile.WriteString(fmt.Sprintf("%s  %s\n", md5Checksum(destTxtPath), txtFileName))
-			}
+			addFileToZip(packageFilename, zipWriter, assembleDir, "meta.yml")
+			addFileToZip(packageFilename, zipWriter, assembleDir, "checksum.md5")
 		}
-
-		addFileToZip(packageFilename, zipWriter, assembleDir, "meta.yml")
-		addFileToZip(packageFilename, zipWriter, assembleDir, "checksum.md5")
 
 		svc.jobDone(js)
 	}()
