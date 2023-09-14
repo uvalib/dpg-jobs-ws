0f9f8257ecd4841c23c932ee41c29deab910b98f
Lou Foster <louffoster@gmail.com>
2023-09-14 15:51:56 -0400

    added csv parse for bond papaers

--- cmd/bondpapers.go
+++ cmd/bondpapers.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"os"
 	"path"
 )
 
@@ -14,12 +16,48 @@ func (svc *ServiceContext) createBondLocations(js *jobStatus, params map[string]
 	}
 	csvPath := path.Join(svc.ProcessingDir, "bondpapers", csvFileName)
 	svc.logInfo(js, fmt.Sprintf("read locations from %s", csvPath))
+	recs, err := readCSV(csvPath)
+	if err != nil {
+		svc.logFatal(js, err.Error())
+	}
+
+	// col 1: title, col 8: BOX/FOLDER, col 9: num pages, col 10: filenames with | sep
+	// Box format: "Box # Folder #"
+	for i, line := range recs {
+		if i == 0 {
+			continue
+		}
+		boxFolder := line[8]
+		svc.logInfo(js, fmt.Sprintf("add location %s", boxFolder))
+	}
 
 	svc.logFatal(js, "Not implemented")
 }
 
 func (svc *ServiceContext) createBondUnits(js *jobStatus, params map[string]interface{}) {
 	svc.logInfo(js, fmt.Sprintf("start script to create units for bond papers"))
+	csvFileName, found := params["fileName"].(string)
+	if found == false {
+		svc.logFatal(js, "missing required fileName param")
+		return
+	}
+	csvPath := path.Join(svc.ProcessingDir, "bondpapers", csvFileName)
+	svc.logInfo(js, fmt.Sprintf("read locations from %s", csvPath))
+	recs, err := readCSV(csvPath)
+	if err != nil {
+		svc.logFatal(js, err.Error())
+	}
+
+	// col 1: title, col 8: BOX/FOLDER, col 9: num pages, col 10: filenames with | sep
+	// Box format: "Box # Folder #"
+	for i, line := range recs {
+		if i == 0 {
+			continue
+		}
+		boxFolder := line[8]
+		title := line[1]
+		svc.logInfo(js, fmt.Sprintf("add unit for %s: %s", boxFolder, title))
+	}
 
 	svc.logFatal(js, "Not implemented")
 }
@@ -28,4 +66,20 @@ func (svc *ServiceContext) ingestBondFolder(js *jobStatus, params map[string]int
 	svc.logInfo(js, fmt.Sprintf("start script to ingest data from one bond folder"))
 
 	svc.logFatal(js, "Not implemented")
+}
+
+func readCSV(filePath string) ([][]string, error) {
+	csvFile, err := os.Open(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("unable to open %s: %s", filePath, err.Error())
+	}
+	defer csvFile.Close()
+
+	csvReader := csv.NewReader(csvFile)
+	csvRecs, err := csvReader.ReadAll()
+	if err != nil {
+		return nil, fmt.Errorf("unable to read csv: %s", err.Error())
+	}
+
+	return csvRecs, nil
 }
